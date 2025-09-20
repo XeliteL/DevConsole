b18c4a7a8147afedecb0ae5434747923b23eb08b
XeliteL8181 <marat140606@gmai.com>
2025-09-20 13:47:15 +0300

    update-readme_help_comms

--- shell/help.go
+++ shell/help.go
@@ -8,8 +8,8 @@ import (
 func printHelp() {
 	fmt.Println("Доступные команды:")
 	fmt.Println("  help       - показать подсказку")
-	fmt.Println("  ls [path]  - показать содержимое(заглушка)")
-	fmt.Println("  cd <path>  - сменить путь(заглушка)")
+	fmt.Println("  ls [path]  - показать содержимое")
+	fmt.Println("  cd <path>  - сменить путь")
 	fmt.Println("  history    - показать историю команд")
 	fmt.Println("  uname      - информация об ОС")
 	fmt.Println("  rmdir <d>  - удаление пустой директории")
